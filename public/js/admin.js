286191f76ce22f5dba16771e04f16af661a07dd4
Deborah606-byte <53974488+Deborah606-byte@users.noreply.github.com>
2023-11-18 15:37:19 +0400

    worked on the admin page

--- public/js/admin.js
+++ public/js/admin.js
@@ -1,12 +1,35 @@
-function showSection(sectionId) {
-  console.log(`Showing section: ${sectionId}`);
-  // Hide all sections
-  const sections = document.querySelectorAll(".admin-popup");
-  sections.forEach((section) => {
-    section.classList.add("hidden");
-  });
+// JavaScript functions to handle section visibility
+function toggleSection(section) {
+  const sectionElement = document.getElementById(section);
 
-  // Show the selected section
-  const selectedSection = document.getElementById(sectionId);
-  selectedSection.classList.remove("hidden");
+  // Toggle the visibility of the selected section
+  sectionElement.classList.toggle("hidden");
+
+  // Hide all other sections
+  hideOtherSections(sectionElement);
 }
+
+function hideOtherSections(excludeSection) {
+  // Hide all sections except the one to exclude
+  const sections = document.querySelectorAll(".dashboard-section");
+  sections.forEach((section) => {
+    const sectionId = section.id;
+    if (sectionId !== excludeSection.id) {
+      section.classList.add("hidden");
+    }
+  });
+}
+
+// Add event listeners for each menu item
+document.addEventListener("DOMContentLoaded", function () {
+  const menuItems = document.querySelectorAll(".dashboard-menu-item");
+
+  menuItems.forEach((item) => {
+    item.addEventListener("click", function () {
+      const sectionToShow = this.getAttribute("data-section");
+
+      // Toggle the visibility of the selected section
+      toggleSection(sectionToShow);
+    });
+  });
+});
