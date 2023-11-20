7062c28d998b1a8c32d6c7e82d90e7798ac2b41c
Deborah606-byte <53974488+Deborah606-byte@users.noreply.github.com>
2023-11-20 11:59:19 +0400

    removed all backend to make the frotend have only the static pages

---
+++ AlumniNexus_frontend/public/js/navbar.js
@@ -0,0 +1,24 @@
+// Add JavaScript to handle dropdown toggle
+document.addEventListener("DOMContentLoaded", function () {
+  const menuButton = document.getElementById("menu-button");
+  const dropdownMenu = document.getElementById("programsEventsDropdown");
+
+  menuButton.addEventListener("click", function () {
+    dropdownMenu.classList.toggle("hidden");
+  });
+
+  // Close dropdown when clicking outside of it
+  document.addEventListener("click", function (event) {
+    if (
+      !menuButton.contains(event.target) &&
+      !dropdownMenu.contains(event.target)
+    ) {
+      dropdownMenu.classList.add("hidden");
+    }
+  });
+});
+
+function toggleMenu() {
+  const navLinks = document.querySelector(".nav-links");
+  navLinks.classList.toggle("hidden");
+}
