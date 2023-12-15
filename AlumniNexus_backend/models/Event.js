0a4b9b54585f2e9c7e917fd130558c1cf2e1779f
Deborah606-byte <53974488+Deborah606-byte@users.noreply.github.com>
2023-12-15 09:29:43 +0400

    changed the name ofthe folder

---
+++ AlumniNexus_backend/models/Event.js
@@ -0,0 +1,59 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+const EventSchema = new Schema({
+  eventCategory: {
+    type: String,
+    required: true,
+    enum: ["None", "Professional Development", "Networking", "Campus Events"],
+  },
+  eventName: {
+    type: String,
+    required: true,
+  },
+  eventSpeaker: {
+    type: [String],
+    required: true,
+  },
+  eventAgenda: {
+    type: String,
+    required: true,
+  },
+  image:{
+    public_id: {
+      type: String,
+      required: true,
+    },
+    url:{
+      type: String,
+      required: true,
+    }
+  },
+  eventDescription: {
+    type: String,
+    required: true,
+  },
+  eventDate: {
+    type: Date,
+    required: true,
+  },
+  eventTime: {
+    type: String,
+    required: true,
+  },
+  eventDuration: {
+    type: Number,
+    required: true,
+  },
+  eventLocation: {
+    type: String,
+    required: true,
+  },
+  userId: {
+    type: Schema.Types.ObjectId,
+    ref: "User", 
+    required: true,
+  }
+});
+
+module.exports = mongoose.model("Event", EventSchema);
